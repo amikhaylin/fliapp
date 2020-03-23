b7c83433cc6d983d0701f9712a08a774b87b359c
Andrey Mikhaylin <cenobith@gmail.com>
2020-03-23 10:33:41 +0300

    Initial project creation

---
+++ fliapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fliapp
+//
+//  Created by Andrey Mikhaylin on 29.02.2020.
+//  Copyright © 2020 Andrey Mikhaylin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
